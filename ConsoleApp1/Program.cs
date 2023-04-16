2a4b1f13c0c73b6677e839d894ff1024eaa99d29
DESKTOP-KC00KL8\Илья <ilarm8q@gmail.com>
2023-04-16 09:24:37 +0300

    Из кода консольного приложения убран лишний код

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,49 +8,16 @@ namespace CombinatorGenerator
 {
     class Program
     {
-        static bool checkResult(List<string> result)
-        {
-            
-            foreach (string element in result)
-            {
-                List<string> combination = result.FindAll(current => current == element);
-                if (combination.Count > 1)
-                {
-                    throw new Exception(
-                        String.Format("комбинация {0} уже существует. Комбинации {1}",
-                        element, 
-                        String.Join(", ", combination.ToArray())
-                    ));
-                }
-            }
-            return true;
-        }
-
         static void Main(string[] args)
         {
             try
             {
-                List<string> result = new List<string>();
-                //CombinationGenerator combinationGenerator = new CombinationGenerator(args[0]);
-                //CombinatorInterface combinationGenerator = CombinatorBuilderDirector.createCombinator("aabc");
-               CombinatorInterface combinationGenerator = CombinatorBuilderDirector.createCombinator("4123");
-
-                List<int> list = new List<int>();
-                list.Add(1);
-                list.Add(2);
-                list.Add(3);
-                list.Add(4);
-                while (combinationGenerator.generateNextCombination(list, list.Count))
+                CombinatorInterface combinationGenerator = CombinatorBuilderDirector.createCombinator(args[0]);
+                do
                 {
-                    Console.WriteLine(string.Format("{0}", list.ToString()));
+                    Console.WriteLine(combinationGenerator.generateUniqueString());
                 }
-/*                do
-                {
-                    result.Add(combinationGenerator.generateUniqueString());
-                    Console.WriteLine(result.Last());
-                }
-                while (!combinationGenerator.printAllCombinations());*/
-                checkResult(result);
+                while (!combinationGenerator.printAllCombinations());
             } 
             catch (Exception exception)
             {
